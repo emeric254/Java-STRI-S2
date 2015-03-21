b46bb3989f801896f50cad5a0a9de9adf76f9f60
emeric254 <emeric254@live.fr>
2015-03-21 22:05:57 +0100

    modifié :         yakuza/MonHistoire.java

--- yakuza/MonHistoire.java
+++ yakuza/MonHistoire.java
@@ -5,10 +5,13 @@ public class MonHistoire
         Humain h = new Humain("Prof", 10, "Porto");
         h.direBonjour();
         h.boire();
+
         Commercant c = new Commercant("Marchant", 35);
         c.direBonjour();
+
         Yakuza y = new Yakuza("Yaku le noir", 42, " biere ", "WarSong");
         y.extorquer(c);
+
         Ronin r = new Ronin("Roro", 61, "sake");
         r.donner(10,c);
         r.provoquer(y);
