50952e33ee27ba3f44f698a74f8bbd43e458161f
emeric254 <emeric254@live.fr>
2015-04-26 16:52:31 +0200

    creation projet maven

---
+++ projet/STRI/src/main/java/com/java_s2/STRI/App.java
@@ -0,0 +1,13 @@
+package com.java_s2.STRI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
