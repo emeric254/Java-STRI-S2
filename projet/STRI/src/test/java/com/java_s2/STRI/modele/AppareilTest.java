a7bab40e95b4b0747559bd8ccb2cbe045c6a8d95
robin <robin@debian>
2015-05-11 17:36:56 +0200

    test firmware

--- projet/STRI/src/test/java/com/java_s2/STRI/modele/AppareilTest.java
+++ projet/STRI/src/test/java/com/java_s2/STRI/modele/AppareilTest.java
@@ -1,7 +1,37 @@
 package com.java_s2.STRI.modele;
 
+import com.java_s2.STRI.AppTest;
+
+import junit.framework.Test;
 import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
-public class AppareilTest extends TestCase {
+public class AppareilTest extends TestCase 
+{
+	/**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppareilTest( String testName )
+    {
+        super( testName );
+    }
 
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppareilTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    Appareil appareil = new Appareil (0, fName, fName, fName, null, null, null);
+    public void testApp()
+    {
+        assertTrue( true );
+    }
 }
