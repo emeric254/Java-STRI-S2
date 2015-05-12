26e6a7c5e288614557a910246ab36d08c0e7ca4f
emeric254 <emeric254@live.fr>
2015-05-12 22:40:56 +0200

    test serial

--- projet/STRI/src/test/java/com/java_s2/STRI/utils/GestionSerialTest.java
+++ projet/STRI/src/test/java/com/java_s2/STRI/utils/GestionSerialTest.java
@@ -1,5 +1,53 @@
 package com.java_s2.STRI.utils;
 
-public class GestionSerialTest {
+import java.util.HashMap;
 
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+
+public class GestionSerialTest extends TestCase
+{
+		private HashMap<Integer, Object> ensemble;
+		
+	    public GestionSerialTest() {
+	        super();
+	        ensemble = new HashMap<Integer, Object>();
+	    }
+
+	    public static Test suite() {
+	        return new TestSuite( GestionSerialTest.class );
+	    }
+
+	    
+	    public void testVide()
+	    {
+	    	assert(GestionSerial.verifExistenceSerial(ensemble, 0));
+	    }
+	    
+	    public void testRemplitUnElement ()
+	    {
+	        ensemble.put(0, "test");
+	    	assert(GestionSerial.verifExistenceSerial(ensemble, 0));
+	    	assertEquals(1, GestionSerial.prochainSerial(ensemble));
+	    }
+	    
+	    public void testRemplitElements ()
+	    {
+	        ensemble.put(0, "test");
+	        ensemble.put(2, "test");
+	        ensemble.put(3, "test");
+	        ensemble.put(4, "test");
+	        ensemble.put(5, "test");
+	    	
+	        assert(GestionSerial.verifExistenceSerial(ensemble, 0x2890));
+	    	
+	    	// car trou de serial entre 0 et le suivant
+	    	assertEquals(1, GestionSerial.prochainSerial(ensemble));
+	    	
+	        ensemble.put(1, "test");
+	        
+	    	assertEquals(6, GestionSerial.prochainSerial(ensemble));
+	    }
 }
