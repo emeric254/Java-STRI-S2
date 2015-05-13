9a5a291c5b7f9958a070255438b900b4f8b01238
remi <remi.b7@hotmail.fr>
2015-05-13 15:28:11 +0200

    activation/desactivation ok

--- projet/STRI/src/test/java/com/java_s2/STRI/modele/AppareilTest.java
+++ projet/STRI/src/test/java/com/java_s2/STRI/modele/AppareilTest.java
@@ -34,52 +34,73 @@ public class AppareilTest extends TestCase
     Firmware firmware = new Firmware(0, "version", "nom");
     InterfaceReseau carteR= new InterfaceReseau(0, "nom", firmware);
     SystemeExploitation os = new SystemeExploitation(0, "nom", "version");
-    Appareil appareil = new Appareil (0, "nom", "marque", "modele", true, os, carteR);
+    
     
     public void testId()
     {
+    	Appareil appareil = new Appareil (0, "nom", "marque", "modele", true, os, carteR);
         assertEquals(0, appareil.getIdAppareil());;
     }
     
     public void testNom ()
     {
+    	Appareil appareil = new Appareil (0, "nom", "marque", "modele", true, os, carteR);
     	assertEquals("nom", appareil.getNomAppareil());
     }
     
     public void testMarque ()
     {
+    	Appareil appareil = new Appareil (0, "nom", "marque", "modele", true, os, carteR);
     	assertEquals("marque", appareil.getMarqueAppareil());
     }
     
     public void testModele ()
     {
+    	Appareil appareil = new Appareil (0, "nom", "marque", "modele", true, os, carteR);
     	assertEquals("modele", appareil.getModeleAppareil());
     }
     
     public void testEtat ()
     {
+    	Appareil appareil = new Appareil (0, "nom", "marque", "modele", true, os, carteR);
     	assertTrue(appareil.getEtatAppareil());
     }
     
     public void testOs ()
     {
+    	Appareil appareil = new Appareil (0, "nom", "marque", "modele", true, os, carteR);
     	assertEquals (os, appareil.getOs());
     }
     
     public void testCarteR ()
     {
+    	Appareil appareil = new Appareil (0, "nom", "marque", "modele", true, os, carteR);
     	assertEquals(carteR, appareil.getInterfaceReseau());
     }
     
     public void testDescativation()
     {
+    	Appareil appareil = new Appareil (0, "nom", "marque", "modele", true, os, carteR);
+    	assertFalse(appareil.desactiver());
+    }
+    
+    public void testReDescativation()
+    {
+    	Appareil appareil = new Appareil (0, "nom", "marque", "modele", false, os, carteR);
     	assertFalse(appareil.desactiver());
     }
     
     public void testActivation()
     {
+    	Appareil appareil = new Appareil (0, "nom", "marque", "modele", false, os, carteR);
     	assertTrue(appareil.activer());
     }
     
+    public void testReActivation()
+    {
+    	Appareil appareil = new Appareil (0, "nom", "marque", "modele", true, os, carteR);
+    	assertTrue(appareil.activer());
+    }
    
+    
 }
