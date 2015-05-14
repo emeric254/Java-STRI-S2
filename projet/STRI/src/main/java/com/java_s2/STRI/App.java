c46333bf9af33b880abec4bf403609f9569ca3d3
emeric254 <emeric254@live.fr>
2015-05-14 22:03:42 +0200

    ajout liaison modele-controlleur

--- projet/STRI/src/main/java/com/java_s2/STRI/App.java
+++ projet/STRI/src/main/java/com/java_s2/STRI/App.java
@@ -1,6 +1,7 @@
 package com.java_s2.STRI;
 
 import com.java_s2.STRI.controller.*;
+import com.java_s2.STRI.modele.Salle;
 import com.java_s2.STRI.vue.*;
 
 /**
@@ -11,7 +12,7 @@ public class App
 {
     public static void main( String[] args )
     {
-    	/**/
+    	/*
     	//
     	createFirmwareWindow fenetre = new createFirmwareWindow();
     	createFirmwareWindowEventListener listener = new createFirmwareWindowEventListener(fenetre);
@@ -31,10 +32,10 @@ public class App
     	createAppareilWindow fenetre = new createAppareilWindow();
     	createAppareilWindowEventListener listener = new createAppareilWindowEventListener(fenetre);
     	//*/
-    	/*
+    	/**/
     	//
     	createSalleWindow fenetre = new createSalleWindow();
-    	createSalleWindowEventListener listener = new createSalleWindowEventListener(fenetre);
+    	createSalleWindowEventListener listener = new createSalleWindowEventListener(fenetre, new Salle(0, ""));
     	//*/
     	/*
     	//
--- projet/STRI/src/main/java/com/java_s2/STRI/controller/createSalleWindowEventListener.java
+++ projet/STRI/src/main/java/com/java_s2/STRI/controller/createSalleWindowEventListener.java
@@ -4,16 +4,19 @@ import java.awt.event.*;
 
 import javax.swing.tree.*;
 
+import com.java_s2.STRI.modele.Salle;
 import com.java_s2.STRI.vue.createSalleWindow;
 
 
 public class createSalleWindowEventListener implements ActionListener
 {
 	private createSalleWindow fenetre;
+	private Salle salle;
 	
-	public createSalleWindowEventListener(createSalleWindow pFenetre)
+	public createSalleWindowEventListener(createSalleWindow pFenetre, Salle pSalle)
 	{
 		fenetre = pFenetre;
+		salle = pSalle;
 		fenetre.getAnnulerBouton().addActionListener(this);
 		fenetre.getCreerBouton().addActionListener(this);
 	}
@@ -22,10 +25,16 @@ public class createSalleWindowEventListener implements ActionListener
 	{
 		Object source = e.getSource();
 		if(source == fenetre.getAnnulerBouton())
-			fenetre.getNomField().setText("annuler !!!");
+		{
+			//@todo add a popup where user have to confirm
+			fenetre.dispose();
+		}
 		else 
 			if(source == fenetre.getCreerBouton())
-				fenetre.getNomField().setText("creeer !!!");
+			{
+				salle.setNomSalle(fenetre.getNomField().getText());
+				fenetre.dispose();
+			}
 	}
 
 }
