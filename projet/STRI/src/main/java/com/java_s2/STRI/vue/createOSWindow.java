941d595b3e91fe36cc7b18e6c97d5219f4434a3c
emeric254 <emeric254@live.fr>
2015-05-14 10:54:15 +0200

    ajout vues

---
+++ projet/STRI/src/main/java/com/java_s2/STRI/vue/createOSWindow.java
@@ -0,0 +1,56 @@
+package com.java_s2.STRI.vue;
+
+import java.awt.*;
+
+import javax.swing.*;
+import javax.swing.tree.*;
+
+public class createOSWindow extends createWindow {
+
+	private JTextField nom;
+	private JTextField version;
+	
+	
+	public createOSWindow()
+	{
+		super("Création de «O/S»");
+		buildContentPane();
+		pack();
+	}
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	
+	private void buildContentPane()
+	{	
+		//creation boutons
+		nom = new JTextField(20);
+		version = new JTextField(20);
+		
+		// definition contraintes du layout
+		GridBagConstraints c = new GridBagConstraints();
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.insets = new Insets(10,15,10,15);
+		c.gridx = 0;
+		c.gridy = 0;
+		getContenuPanel().add(new JLabel("nom"), c);
+		c.gridx = 1;
+		getContenuPanel().add(nom, c);
+		
+		c.gridx = 0;
+		c.gridy = 1;
+		getContenuPanel().add(new JLabel("version"), c);
+		c.gridx = 1;
+		getContenuPanel().add(version, c);
+	}
+
+	public JTextField getNomField() {
+		return nom;
+	}
+
+	public JTextField getVersionField() {
+		return version;
+	}
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+}
