fcdeb6e3e8284341cfd8b9cf2e7e0780eab48aa8
robin <robin_mijeanne@hotmail.fr>
2015-05-19 10:36:28 +0200

    test connexion bd

--- projet/STRI/src/test/java/com/java_s2/STRI/utils/PostgreSQLTest.java
+++ projet/STRI/src/test/java/com/java_s2/STRI/utils/PostgreSQLTest.java
@@ -1,7 +1,28 @@
 package com.java_s2.STRI.utils;
 
+import com.java_s2.STRI.modele.AppareilTest;
+
+import junit.framework.Test;
 import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
 public class PostgreSQLTest extends TestCase {
+	/**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public PostgreSQLTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( PostgreSQLTest.class );
+    }
 
 }
