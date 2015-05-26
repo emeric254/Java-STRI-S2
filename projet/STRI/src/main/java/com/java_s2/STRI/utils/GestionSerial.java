04ab6deb61a93c7f3a27448a398ce95696979c4b
robin <robin_mijeanne@hotmail.fr>
2015-05-26 15:04:26 +0200

    comment javadoc GestionSerial.java

--- projet/STRI/src/main/java/com/java_s2/STRI/utils/GestionSerial.java
+++ projet/STRI/src/main/java/com/java_s2/STRI/utils/GestionSerial.java
@@ -2,13 +2,31 @@ package com.java_s2.STRI.utils;
 
 import java.util.*;
 
+/**
+ * Classe qui gere les numeros d identifiant des classes
+ * 
+ * @author robin
+ *
+ */
 public class GestionSerial {
 
+    /**
+     * Verifie l'existance d'un serial dans la liste set
+     * 
+     * @param set
+     * @param Serial
+     * @return Boolean 
+     */
     public static boolean verifExistenceSerial(Set<Integer> set, int Serial)
     {
         return set.contains(Serial);
     }
 
+    /**
+     * retourne le prochain identifiant numerique correspondant a la set
+     * @param set
+     * @return int
+     */
     public static int prochainSerial(Set<Integer> set)
     {
         int Serial = 0;
