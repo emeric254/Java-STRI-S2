2fe0fe37a873285e5ca9ebf4d11bec4c719e7e9d
robin <robin_mijeanne@hotmail.fr>
2015-05-26 14:58:28 +0200

    comment javadoc gestionMac.java

--- projet/STRI/src/main/java/com/java_s2/STRI/utils/GestionMAC.java
+++ projet/STRI/src/main/java/com/java_s2/STRI/utils/GestionMAC.java
@@ -2,18 +2,40 @@ package com.java_s2.STRI.utils;
 
 import java.util.*;
 
+/**
+ * @author robin
+ *
+ */
 public class GestionMAC {
 
+	/**
+	 * Verifie l'existence de l'adresse mac dans la Set ensemble
+	 * 
+	 * @param ensemble
+	 * @param addrMAC
+	 * @return
+	 */
 	public static boolean verifExistenceMAC(Set<Integer> ensemble, int addrMAC)
 	{
 		return GestionSerial.verifExistenceSerial(ensemble,addrMAC);
 	}
 	
+	/**
+	 * Genere la prochaine adresse mac unique dans le programme
+	 * 
+	 * @param ensemble
+	 * @return
+	 */
 	public static int prochainMAC(Set<Integer> ensemble)
 	{
 		return GestionSerial.prochainSerial(ensemble);
 	}
 	
+	/**
+	 * Retourne une adresse mac au format chaine FF:FF:FF:FF:FF:FF
+	 * @param addrMAC
+	 * @return
+	 */
 	public static String addrMACString(int addrMAC)
 	{
 		String chaine = Integer.toHexString(addrMAC).toUpperCase();
